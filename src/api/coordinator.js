a73c440a4bc792d5567eb25a0d7fb5fd17c8bdc6
Vitor <vitorbertulucci@gmail.com>
2017-05-05 22:05:39 -0300

    [COORDINATOR] Added get all and delete by id methods for coordinator controller.
    Signed-off-by: Danilo <dan.b412@gmail.com>
    Signed-off-by: Vitor <vitorbertulucci@gmail.com>

--- src/api/coordinator.js
+++ src/api/coordinator.js
@@ -4,5 +4,31 @@ import Coordinator from '../models/coordinator'
 export default ({ config, db }) => {
   let router = Router()
 
+  router.param('coordinator', (request, response, next, id) => {
+    request.coordinator = Coordinator.get(id)
+  })
+
+  router.get('/', async (request, response) => {
+    try {
+      response.json(await Coordinator.orderBy('name').run())
+    } catch (error) {
+      response.status(404).json({error: error})
+    }
+  })
+
+  router.delete('/', async (request, response) => {
+    try {
+      console.log(request.body)
+      if (request.body.id !== undefined) {
+        var deletion = await Coordinator.get(request.body.id).delete().run()
+        response.json(deletion)
+      } else {
+        response.status(500).json({error: 'error'})
+      }
+    } catch (error) {
+      response.status(404).json({error: error})
+    }
+  })
+
   return router
 }
