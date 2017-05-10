78a4fdc2f3614512709142c7c53f94fa9ce10b6d
DaniloBarros <dan.b412@gmail.com>
2017-05-10 19:56:10 -0300

    [MODEL FORUM] Fixing forum model to be compatible with local database
    Signed-off-by: DaniloBarros <dan.b412@gmail.com>
    Signed-off-by: Felipe Hargreaves <fhargreaves00@gmail.com>

--- src/models/forum.js
+++ src/models/forum.js
@@ -2,13 +2,15 @@ import { thinky, type } from '../db'
 import Coordinator from './coordinator'
 
 let Forum = thinky.createModel('Forum', {
-  theme: [type.string()],
+  theme: type.string(),
   date: type.date().required(),
   hour: type.number(),
-  place: {
-    room: type.string(),
-    building: type.string()
-  }
+  schedules: type.string(),
+  place: type.string()
+  // place: {
+  //   room: type.string(),
+  //   building: type.string()
+  // }
 })
 
 export default Forum
