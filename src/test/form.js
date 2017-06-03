5d23e59a8a06d459d071072ac70f86611cd58dbe
DaniloBarros <dan.b412@gmail.com>
2017-06-03 18:53:44 -0300

    Fixing post and put, adding tests for form
    Signed-off-by: Mariana Pícolo <marianapicolo@hotmail.com>
    Signed-off-by: Arthur Assis <arthur120496@gmail.com>
    Signed-off-by: Danilo Barros <dan.b412@gmail.com>

--- src/test/form.js
+++ src/test/form.js
@@ -4,6 +4,7 @@ import chaiHttp from 'chai-http'
 import * as seed from '../seeds/seeds'
 
 let should = chai.should()
+let expect = chai.expect()
 
 chai.use(chaiHttp)
 
@@ -113,6 +114,68 @@ describe("Form Tests", function () {
       })
     })
 
+    it('it should post a form with date in format MM/DD/YYYY', (done) => {
+      let form = {
+        form: {
+          title: "Formulário 6",
+          expiration_date: "06/03/2017",
+          discussive: [{
+            question: "Questão 1?",
+            answer: "Resposta 1"
+          }],
+          multipleChoices: [{
+            question: "Questão 2?",
+            options: ["A", "B", "C", "D"],
+            answer: "A"
+          }]
+        }
+      }
+
+      chai.request(runningServer)
+      .post('/api/forms')
+      .send(form)
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        res.body.result.should.have.property('title')
+        res.body.result.should.have.property('expiration_date')
+        res.body.result.should.have.property('discussive')
+        res.body.result.should.have.property('multipleChoices')
+        res.body.result.should.have.property('id')
+        res.body.success.should.be.eql(true)
+        done()
+      })
+    })
+
+    it('it should not post a form with date in format DD/MM/YYYY', (done) => {
+      let form = {
+        form: {
+          title: "Formulário 6",
+          expiration_date: "26/12/2017",
+          discussive: [{
+            question: "Questão 1?",
+            answer: "Resposta 1"
+          }],
+          multipleChoices: [{
+            question: "Questão 2?",
+            options: ["A", "B", "C", "D"],
+            answer: "A"
+          }]
+        }
+      }
+
+      chai.request(runningServer)
+      .post('/api/forms')
+      .send(form)
+      .end((err, res) => {
+        res.should.have.status(404)
+        res.body.should.be.a('object')
+        res.body.should.have.property('error')
+        res.body.success.should.be.eql(false)
+        done()
+      })
+    })
+
     it('it should update a form given the id', (done) => {
       chai.request(runningServer)
       .put('/api/forms/1')
@@ -121,6 +184,32 @@ describe("Form Tests", function () {
         res.should.have.status(200)
         res.body.should.be.a('object')
         res.body.result.should.have.property('id').eql('1')
+        done()
+      })
+    })
+
+    it('it should update a form given the expiration date', (done) => {
+      chai.request(runningServer)
+      .put('/api/forms/1')
+      .send({form: {expiration_date: '12/20/2018'}})
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('object')
+        var date = new Date('12/20/2018')
+        res.body.result.should.have.property('expiration_date')
+        done()
+      })
+    })
+
+    it('it should not update a form given an invalid date', (done) => {
+      chai.request(runningServer)
+      .put('/api/forms/1')
+      .send({form: {expiration_date: '20/12/2018'}})
+      .end((err, res) => {
+        res.should.have.status(404)
+        res.body.should.be.a('object')
+        res.body.should.have.property('error')
+        res.body.success.should.be.eql(false)
         done()
       })
     })
@@ -134,6 +223,6 @@ describe("Form Tests", function () {
         done()
       })
     })
-    
+
   })
 })
